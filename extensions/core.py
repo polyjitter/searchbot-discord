ec084dcb1fb32e1abdb6dc6d180ae7e553180b25
Adriene Hutchins <ustin.a.h+github@gmail.com>
2020-03-02 15:20:53 -0500

    Even more about formatting

--- extensions/core.py
+++ extensions/core.py
@@ -75,11 +75,11 @@ class Core(commands.Cog):
     async def about(self, ctx):
         """Returns information about this bot."""
 
-        msg = f"__**{self.bot.user.name}**__ - _{self.bot.description}_\n"
+        msg = f"__**{self.bot.user.name}**__ - _{self.bot.description}_\n\n"
         msg += f"This instance by **{self.bot.appinfo.owner}.**\n\n"
         msg += "**Source Code:** _<https://github.com/taciturasa/searchbot-discord>_\n"
         msg += "**Support Server:** _<https://discord.gg/4BpReNV>_\n"
-        msg += "_Note: Please attempt to contact the hoster of any separate instances before this server._\n\n"
+        msg += "_Note: Please attempt to contact the hoster of any separate instances before this server._\n"
         msg += f"_See **{ctx.prefix}**`help` for help, `invite` to add the bot, and `stats` for statistics._"
 
         await ctx.send(msg)

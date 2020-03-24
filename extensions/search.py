5f8ef8a8d8682a32c37b869859b3a28f9e2c2451
Adriene Hutchins <ustin.a.h+github@gmail.com>
2020-03-24 19:36:10 -0400

    Even more formatting

--- extensions/search.py
+++ extensions/search.py
@@ -167,7 +167,7 @@ class Search(commands.Cog, name="Basic"):
             # Sends message
             await self.info(
                 f"**New Search** - `{ctx.author}` in `{ctx.guild}`\n\n{msg}",
-                name="_basic_search"
+                name="New Search"
             )
             await ctx.send(msg)
 
